9856fc4834d257be0cc0b1a0709e005ce6ee1b5f
John Stassen <john.stassen@gmail.com>
2020-10-24 23:53:52 +0000

    add description

---
+++ lib/sdg_explorer.rb
@@ -0,0 +1,6 @@
+require "sdg_explorer/version"
+
+module SdgExplorer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sdg_explorer/version.rb
@@ -0,0 +1,3 @@
+module SdgExplorer
+  VERSION = "0.1.0"
+end
